fe6f1724dbddb937c8ffcb49b9700a64d816c817
gcallah <gcallah@mac.com>
2022-10-06 12:00:39 -0400

    Wrote db code for add game.

--- db/games.py
+++ db/games.py
@@ -5,11 +5,20 @@ This module encapsulates details about games.
 
 TEST_GAME_NAME = 'Test game'
 NUM_PLAYERS = 'num_players'
+LEVEL = 'level'
+VIOLENCE = 'violence'
 
 # We expect the game database to change frequently:
-games = {TEST_GAME_NAME: {NUM_PLAYERS: 7, 'level': 10},
-         'game2': {NUM_PLAYERS: 9, 'level': 9},
-         'game3': {NUM_PLAYERS: 6, 'level': 1}, }
+# For now, we will consider NUM_PLAYERS and LEVEL to be
+# our mandatory fields.
+REQUIRED_FLDS = [NUM_PLAYERS, LEVEL, VIOLENCE]
+games = {TEST_GAME_NAME: {NUM_PLAYERS: 7, LEVEL: 10, VIOLENCE: 2},
+         'game2': {NUM_PLAYERS: 9, LEVEL: 9, VIOLENCE: 2},
+         'game3': {NUM_PLAYERS: 6, LEVEL: 1, VIOLENCE: 2}, }
+
+
+def game_exists(name):
+    return name in games
 
 
 def get_games():
@@ -18,6 +27,17 @@ def get_games():
 
 def get_game_details(game):
     return games.get(game, None)
+
+
+def add_game(name, details):
+    if not isinstance(name, str):
+        raise TypeError(f'Wrong type for name: {type(name)=}')
+    if not isinstance(details, dict):
+        raise TypeError(f'Wrong type for details: {type(details)=}')
+    for field in REQUIRED_FLDS:
+        if field not in details:
+            raise ValueError(f'Required {field=} missing from details.')
+    games[name] = details
 
 
 def main():
