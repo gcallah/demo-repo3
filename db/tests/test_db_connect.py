49c9f35859609017e68e5bb5128863e1d6a01fe8
gcallah <gcallah@mac.com>
2022-11-22 11:51:57 -0500

    Took out all CI checks from test_db_connect.

--- db/tests/test_db_connect.py
+++ db/tests/test_db_connect.py
@@ -16,23 +16,18 @@ TEST_NAME = 'test'
 
 @pytest.fixture(scope='function')
 def temp_rec():
-        dbc.connect_db()
-        dbc.client[TEST_DB][TEST_COLLECT].insert_one({TEST_NAME: TEST_NAME})
-        # yield to our test function
-        yield
-        dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
-    # else:
-    #     yield
-    #     return True
+    dbc.connect_db()
+    dbc.client[TEST_DB][TEST_COLLECT].insert_one({TEST_NAME: TEST_NAME})
+    # yield to our test function
+    yield
+    dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
 
 
 def test_fetch_one(temp_rec):
-    if not RUNNING_ON_CICD_SERVER:
-        ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
-        assert ret is not None
+    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
+    assert ret is not None
 
 
 def test_fetch_one_not_there(temp_rec):
-    if not RUNNING_ON_CICD_SERVER:
-        ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
-        assert ret is None
+    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
+    assert ret is None
