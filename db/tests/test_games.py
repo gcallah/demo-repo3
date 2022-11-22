4858558a966bfcc23f9f94f4d78090fc92e19671
gcallah <gcallah@mac.com>
2022-11-22 11:55:36 -0500

    All CI checks out of test_games.py.

--- db/tests/test_games.py
+++ db/tests/test_games.py
@@ -17,65 +17,52 @@ def create_game_details():
 
 @pytest.fixture(scope='function')
 def temp_game():
-    if not RUNNING_ON_CICD_SERVER:
-        gm.add_game(gm.TEST_GAME_NAME, create_game_details())
-        yield
-        return True
-        # gm.del_game(gm.TEST_GAME_NAME)
-    else:
-        yield
-        return True
+    gm.add_game(gm.TEST_GAME_NAME, create_game_details())
+    yield
+    return True
+    # gm.del_game(gm.TEST_GAME_NAME)
 
 
 def test_get_games():
-    if not RUNNING_ON_CICD_SERVER:
-        gms = gm.get_games()
-        assert isinstance(gms, list)
-        assert len(gms) > 1
+    gms = gm.get_games()
+    assert isinstance(gms, list)
+    assert len(gms) > 1
 
 
 def test_get_games_dict():
-    if not RUNNING_ON_CICD_SERVER:
-        gms = gm.get_games_dict()
-        assert isinstance(gms, dict)
-        assert len(gms) > 1
+    gms = gm.get_games_dict()
+    assert isinstance(gms, dict)
+    assert len(gms) > 1
 
 
 def test_get_game_details(temp_game):
-    if not RUNNING_ON_CICD_SERVER:
-        gm_dtls = gm.get_game_details(gm.TEST_GAME_NAME)
-        assert isinstance(gm_dtls, dict)
+    gm_dtls = gm.get_game_details(gm.TEST_GAME_NAME)
+    assert isinstance(gm_dtls, dict)
 
 
 def test_game_exists(temp_game):
-    if not RUNNING_ON_CICD_SERVER:
-        assert gm.game_exists(gm.TEST_GAME_NAME)
+    assert gm.game_exists(gm.TEST_GAME_NAME)
 
 
 def test_game_not_exists():
-    if not RUNNING_ON_CICD_SERVER:
-        assert not gm.game_exists('Surely this is not a game name!')
+    assert not gm.game_exists('Surely this is not a game name!')
 
 
 def test_add_wrong_name_type():
-    if not RUNNING_ON_CICD_SERVER:
-        with pytest.raises(TypeError):
-            gm.add_game(7, {})
+    with pytest.raises(TypeError):
+        gm.add_game(7, {})
 
 
 def test_add_wrong_details_type():
-    if not RUNNING_ON_CICD_SERVER:
         with pytest.raises(TypeError):
             gm.add_game('a new game', [])
 
 
 def test_add_missing_field():
-    if not RUNNING_ON_CICD_SERVER:
-        with pytest.raises(ValueError):
-            gm.add_game('a new game', {'foo': 'bar'})
+    with pytest.raises(ValueError):
+        gm.add_game('a new game', {'foo': 'bar'})
 
 
 def test_add_game():
-    if not RUNNING_ON_CICD_SERVER:
-        gm.add_game(gm.TEST_GAME_NAME, create_game_details())
+    gm.add_game(gm.TEST_GAME_NAME, create_game_details())
     # assert gm.game_exists(gm.TEST_GAME_NAME)
