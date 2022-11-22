05cc40f57fb167be9ee6f32432312c365701dfa0
gcallah <gcallah@mac.com>
2022-11-22 11:49:00 -0500

    Trying to use GH Actions local mongo instance.

--- db/tests/test_db_connect.py
+++ db/tests/test_db_connect.py
@@ -16,15 +16,15 @@ TEST_NAME = 'test'
 
 @pytest.fixture(scope='function')
 def temp_rec():
-    if not RUNNING_ON_CICD_SERVER:
         dbc.connect_db()
         dbc.client[TEST_DB][TEST_COLLECT].insert_one({TEST_NAME: TEST_NAME})
         # yield to our test function
         yield
         dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
-    else:
-        yield
-        return True
+    # else:
+    #     yield
+    #     return True
+
 
 def test_fetch_one(temp_rec):
     if not RUNNING_ON_CICD_SERVER:
