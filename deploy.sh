ab69721d01ac377db9f7153bbbe2d844fc727345
gcallah <gcallah@mac.com>
2022-12-12 17:38:00 -0500

    Got PA connecting to MongoDB.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This shell script deploys a new version to a server.
+
+export PA_USER=SWEDemo
+
+if [ -z "$DEMO_PA_PWD" ]
+then
+    echo "The PythonAnywhere password var (DEMO_PA_PWD) must be set in the env."
+    exit 1
+fi
+
+echo "SSHing to PythonAnywhere."
+sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
+    ./rebuild.sh
+EOF
