c6c10ea5ab81c1ef96eb968826042bdb842a6335
Gene Callahan <gcallah@mac.com>
2022-12-16 10:58:31 -0500

    cd into project dir before running rebuild.sh

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ fi
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    ./rebuild.sh
+    cd demo-repo3; ./rebuild.sh
 EOF
